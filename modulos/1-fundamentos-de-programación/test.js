a13c91b2a191abd8dbb2aaec63db703f12878586
Ema Lorenzo <aemalorenzo@gmail.com>
2021-05-23 20:09:49 -0300

    clase 5

---
+++ modulos/1-fundamentos-de-programación/test.js
@@ -0,0 +1,2 @@
+console.log('oeoe')
+console.log('wachin')
---
+++ theme.js
@@ -0,0 +1,11 @@
+export default {
+  fonts: {
+    body: 'Poppins, sans-serif',
+    monospace: '"Poppins", monospace',
+  },
+  colors: {
+    text: 'white',
+    background: '#121212',
+    primary: 'blue',
+  },
+}
